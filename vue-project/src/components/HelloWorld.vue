807e995124b59949c3da85842d271473953060dc
Marxu <fuhaojun@52tt.com>
2024-02-21 10:37:01 +0800

    feat: 使用 ref 响应数据 + 使用nextTick

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -1,15 +1,48 @@
 <script setup>
+import { nextTick, ref } from "vue";
+
 defineProps({
   msg: {
     type: String,
     required: true
   }
 })
+
+// 一般响应
+const count = ref(0);
+
+console.log(count.value) // 0 
+
+
+// 深度响应
+const obj = ref({
+    nested: {count: 0},
+    arr :['foo', 'bar']
+})
+
+console.log(obj.value.nested.count +1) //1
+console.log(obj.value.arr.push('ccc'));
+console.log(obj.value.arr); // Proxy(Array) {0: 'foo', 1: 'bar', 2: 'ccc'}
+
+// 点击按钮时增加 count
+// const incrementCount = () => {
+//   count.value++;
+// };
+
+// DOM update to complete after a state change
+async function increment() {
+  count.value++
+  await nextTick()
+  // Now the DOM is updated
+}
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <div>
+        <button @click="increment">{{ count }}</button>
+    </div>
   </div>
 </template>
 
